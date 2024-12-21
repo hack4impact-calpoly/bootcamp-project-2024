2ff4aa86b795865100f55eb551458209705bc9e4
Brady Welsh <blwelsh@calpoly.edu>
2024-12-21 17:36:09 -0500

    feat: POST for comments to a post

--- bootcamp-milestone-2/src/app/api/blogs/[slug]/route.ts
+++ bootcamp-milestone-2/src/app/api/blogs/[slug]/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import connectDB from "@/database/db";
 import blogSchema from "@/database/blogSchema";
+import { IComment } from "@/database/blogSchema";
 
 type IParams = {
     params: {
@@ -12,9 +13,18 @@ export async function GET(req: NextRequest, { params }: IParams) {
     await connectDB();
 
     const { slug } = params;
+    const comment: IComment = req.body;
+
 
     try {
-        const blog = await blogSchema.findOne({slug}).orFail();
+        // Get the blog we are adding a comment to and push the comment onto it
+        const blog = await blogSchema.findOneAndUpdate({slug}, 
+            {$push: {"comments": comment}}
+        ).orFail();
+
+        
+
+
         return NextResponse.json(blog);
     } catch (err) {
         console.log(err);
