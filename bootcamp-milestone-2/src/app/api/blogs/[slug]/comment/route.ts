cbf7ad4f7394b754a717d989fe2c6f89a56a5abc
Brady Welsh <blwelsh@calpoly.edu>
2024-12-21 17:37:48 -0500

    fix: endpoints corrected (get and post were initially mixed)

--- bootcamp-milestone-2/src/app/api/blogs/[slug]/comment/route.ts
+++ bootcamp-milestone-2/src/app/api/blogs/[slug]/comment/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import connectDB from "@/database/db";
 import blogSchema from "@/database/blogSchema";
+import { IComment } from "@/database/blogSchema";
+
 
 type IParams = {
     params: {
@@ -10,16 +12,26 @@ type IParams = {
 
 export async function POST(req: NextRequest, { params }: IParams) {
     await connectDB();
-    const {slug} = params;
+
+    const { slug } = params;
+    const comment: IComment = req.body;
+
 
     try {
-        const blog = await blogSchema.findOne({slug}).orFail();
+        // Get the blog we are adding a comment to and push the comment onto it
+        const blog = await blogSchema.findOneAndUpdate({slug}, 
+            {$push: {"comments": comment}}
+        ).orFail();
+
+        
+
+
         return NextResponse.json(blog);
-
-
     } catch (err) {
         console.log(err);
         return NextResponse.json("Blog not found.", { status: 404});
     }
+    
 
 }
+
--- bootcamp-milestone-2/src/app/api/blogs/[slug]/route.ts
+++ bootcamp-milestone-2/src/app/api/blogs/[slug]/route.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import connectDB from "@/database/db";
 import blogSchema from "@/database/blogSchema";
-import { IComment } from "@/database/blogSchema";
 
 type IParams = {
     params: {
@@ -11,21 +10,13 @@ type IParams = {
 
 export async function GET(req: NextRequest, { params }: IParams) {
     await connectDB();
-
-    const { slug } = params;
-    const comment: IComment = req.body;
-
+    const {slug} = params;
 
     try {
-        // Get the blog we are adding a comment to and push the comment onto it
-        const blog = await blogSchema.findOneAndUpdate({slug}, 
-            {$push: {"comments": comment}}
-        ).orFail();
-
-        
-
-
+        const blog = await blogSchema.findOne({slug}).orFail();
         return NextResponse.json(blog);
+
+
     } catch (err) {
         console.log(err);
         return NextResponse.json("Blog not found.", { status: 404});
