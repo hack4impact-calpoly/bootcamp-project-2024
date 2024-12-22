78333f6ba93f6c1834330482499cac58bf2ed26f
nickaan04 <nickan04@gmail.com>
2024-12-21 23:27:37 -0800

    add comment component

---
+++ bootcamp-milestone-2/src/components/Comment/Comment.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import style from "./comment.module.css";
+
+type CommentProps = {
+  comment: IComment;
+};
+
+function parseCommentTime(time: Date) {
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true
+  };
+  return new Date(time).toLocaleString("en-US", options);
+}
+
+function Comment({ comment }: CommentProps) {
+  return (
+    <div className={style.commentContainer}>
+      <h4 className={style.commentUser}>{comment.user}</h4>
+      <p className={style.commentText}>{comment.comment}</p>
+      <span className={style.commentTime}>
+        {parseCommentTime(comment.time)}
+      </span>
+    </div>
+  );
+}
+
+export default Comment;
