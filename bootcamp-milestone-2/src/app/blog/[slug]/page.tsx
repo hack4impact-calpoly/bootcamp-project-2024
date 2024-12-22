d70b20604aa74ee2300ba617289e270272e0e9c8
nickaan04 <nickan04@gmail.com>
2024-12-21 23:09:52 -0800

    fetching specific project

--- bootcamp-milestone-2/src/app/blog/[slug]/page.tsx
+++ bootcamp-milestone-2/src/app/blog/[slug]/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Image from "next/image";
-import BlogModel from "@/database/blogSchema";
 import style from "@/components/BlogPreview/blogPreview.module.css";
 
 type Props = {
--- bootcamp-milestone-2/src/app/portfolio/[slug]/page.tsx
+++ bootcamp-milestone-2/src/app/portfolio/[slug]/page.tsx
@@ -1,9 +1,50 @@
 import React from "react";
 import Image from "next/image";
-import ProjectModel from "@/database/projectSchema";
+import style from "@/components/Portfolio/portfolio.module.css";
 
 type Props = {
   params: { slug: string };
 };
 
-export default async function Portfolio({ params: { slug } }: Props) {}
+async function getProject(slug: string) {
+  try {
+    const res = await fetch(`http://localhost:3000/api/projects/${slug}`, {
+      cache: "no-store"
+    });
+    if (!res.ok) {
+      throw new Error("Failed to fetch blog");
+    }
+
+    return res.json();
+  } catch (err: unknown) {
+    console.log(`error: ${err}`);
+    return null;
+  }
+}
+
+export default async function Portfolio({ params: { slug } }: Props) {
+  const project = await getProject(slug);
+  return (
+    <main>
+      {project != null ? (
+        <div>
+          <h1 className="page-title">{project.name}</h1>
+          <div>
+            <Image
+              className={style.projectImage}
+              src={project.image}
+              alt={project.image_alt}
+              width={600}
+              height={500}
+            />
+            <div>
+              <p>{project.description}</p>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <h1 className="page-title">404 - Project Not Found</h1>
+      )}
+    </main>
+  );
+}
