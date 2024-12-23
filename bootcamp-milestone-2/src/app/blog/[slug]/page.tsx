9f18397a4c563dbdf440a05d6d7e1802fe36cf1d
Brady Welsh <blwelsh@calpoly.edu>
2024-12-22 23:28:27 -0500

    fix: added NEXT_PUBLIC to env

--- bootcamp-milestone-2/src/app/blog/[slug]/page.tsx
+++ bootcamp-milestone-2/src/app/blog/[slug]/page.tsx
@@ -40,7 +40,7 @@ async function postComment(slug: string, comment: IComment){
 async function getBlog(slug: string) {
 
     try {
-        const url = process.env.VERCEL_URL ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` : `http://localhost:3000`;
+        const url = process.env.NEXT_PUBLIC_VERCEL_URL ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` : `http://localhost:3000`;
         const res = await fetch(`${url}/api/blogs/${slug}`, {
             cache: "no-store",
         })
