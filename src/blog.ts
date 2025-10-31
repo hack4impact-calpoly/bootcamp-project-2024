58eb2c6d58493e145f1a218bad35898c53a56775
Jacqueline B <jlinnieb@gmail.com>
2025-10-30 23:03:00 -0700

    made extra changes to blog.ts and js so that they work together better

--- src/blog.ts
+++ src/blog.ts
@@ -1,44 +1,56 @@
+// Define the shape of a blog post
 type Blog = {
   title: string;
   date: string;
   description: string;
-  image: string;      
+  image: string;
   imageAlt: string;
   slug: string;
 };
 
+// Blog data
 const blogs: Blog[] = [
   {
     title: "My First Blog Post",
     date: "2025-10-28",
-    description: "This is my first blog post where I share what I learned starting the bootcamp.",
-    image: "./images/first-post.png",
-    imageAlt: "Screenshot of my first blog project",
-    slug: "my-first-blog-post"
+    description: "This is my first blog post where I talk about blog stuff!",
+    image: "./images/tennisbird.webp",
+    imageAlt: "Tennis bird",
+    slug: "my-first-blog-post",
   },
   {
-    title: "Learning TypeScript",
+    title: "My Second Blog Post",
     date: "2025-10-27",
-    description: "A quick overview of how TypeScript makes JavaScript safer and easier to maintain.",
+    description: "This is my second blog post where I talk about more blog stuff",
     image: "./images/learning-ts.png",
-    imageAlt: "TypeScript logo and code snippet",
-    slug: "learning-typescript"
-  }
+    imageAlt: "second image",
+    slug: "learning-typescript",
+  },
+  {
+    title: "My Third Blog Post",
+    date: "2025-10-27",
+    description: "This is my third blog post where I talk about more blog stuff",
+    image: "./images/learning-ts.png",
+    imageAlt: "third image",
+    slug: "learning-typescript",
+  },
 ];
 
-document.addEventListener('DOMContentLoaded', () => { // safety measure 
-  const container = document.getElementById('blog-container');
+// On load, populate the blog list ONLY if we're on blog.html
+// (post.html won't have #blog-container, so this won't run there)
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.getElementById("blog-container");
+
   if (container) {
     blogs.forEach((blog) => {
-      const div = document.createElement('div');
-      div.className = 'blog-card';
+      const div = document.createElement("div");
+      div.className = "blog-card";
       div.innerHTML = `
-  <a href="./blogs/${blog.slug}.html" class="blog-link">
-    <h2>${blog.title}</h2>
-    <p>${blog.date}</p>
-    <img src="${blog.image}" alt="${blog.imageAlt}">
-    <p>${blog.description}</p>
-  </a>
+        <a href="./blogs/post.html?slug=${blog.slug}" class="blog-link">
+          <h2>${blog.title}</h2>
+          <p>${blog.date}</p>
+          <img src="${blog.image}" alt="${blog.imageAlt}">
+        </a>
       `;
       container.appendChild(div);
     });
